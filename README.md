bc88a6adc9991980080b417924b07d97f9f60efb
Alexander Mostovenko <alexmost1989@gmail.com>
2017-12-07 00:16:13 +0200

    use NameAllModulesPlugin

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 2. Add `[chunkhash]` to the resulting `output.filename` and `output.chunkFilename`.
 3. Create runtime chunk to store all chunknames.
 4. Use `NamedModulesPlugin`.
+5. Use `NamedChunksPlugin`.
+6. Use `NameAllModulesPlugin`.
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const NameAllModulesPlugin = require('name-all-modules-plugin');
 
 const isVendor = (opts) => (
     opts.userRequest && opts.userRequest.indexOf('node_modules') >= 0
@@ -21,6 +22,7 @@ module.exports = {
         new webpack.optimize.CommonsChunkPlugin({
             name: 'runtime'
         }),
+        // plugins for proper chunks naming
         new webpack.NamedChunksPlugin((chunk) => {
             if (chunk.name) { 
                 return chunk.name; 
@@ -28,5 +30,6 @@ module.exports = {
             return chunk.modules.map(m => path.relative(m.context, m.request)).join("_");
         }),
         new webpack.NamedModulesPlugin(),
+        new NameAllModulesPlugin(),
     ]
 };
